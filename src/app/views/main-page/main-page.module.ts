cc9b3d66a5a50fe152ae1cf09ebadc32f87dbaa0
Artem Katrusha <artemkatrusha@gmail.com>
2019-11-01 04:19:43 +0300

    moar restructurizing

--- src/app/views/main-page/main-page.module.ts
+++ src/app/views/main-page/main-page.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page.component';
-import { SliderComponent } from './slider/slider.component';
-import { PopularComponent } from './popular/popular.component';
-import { CategoriesComponent } from './categories/categories.component';
-import { NewGoodsComponent } from './new-goods/new-goods.component';
 import { GadgetsComponent } from './gadgets/gadgets.component';
-import { RecentComponent } from './recent/recent.component';
 import { SeoComponent } from './seo/seo.component';
 import { GadgetCardComponent } from './gadget-card/gadget-card.component';
 import { ProductCardComponent } from './product-card/product-card.component';
@@ -15,12 +10,7 @@ import { MainPageRoutingModule } from '@views/main-page/main-page-routing.module
 
 const components = [
     MainPageComponent,
-    SliderComponent,
-    PopularComponent,
-    CategoriesComponent,
-    NewGoodsComponent,
     GadgetsComponent,
-    RecentComponent,
     SeoComponent,
     GadgetCardComponent,
     ProductCardComponent,
