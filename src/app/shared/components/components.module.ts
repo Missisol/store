0639056c45f95dfdc3d228e5be2244f245fa2609
MarinaOl <missis_ol@mail.ru>
2019-11-10 04:04:08 +0300

    Truncate pipe added

--- src/app/shared/components/components.module.ts
+++ src/app/shared/components/components.module.ts
@@ -1,34 +1,38 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderTopComponent } from './header/header-top/header-top.component';
-import { HeaderBottomComponent } from './header/header-bottom/header-bottom.component';
-import { FooterTopComponent } from './footer/footer-top/footer-top.component';
-import { FooterBottomComponent } from './footer/footer-bottom/footer-bottom.component';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeaderComponent} from './header/header.component';
+import {FooterComponent} from './footer/footer.component';
+import {HeaderTopComponent} from './header/header-top/header-top.component';
+import {HeaderBottomComponent} from './header/header-bottom/header-bottom.component';
+import {FooterTopComponent} from './footer/footer-top/footer-top.component';
+import {FooterBottomComponent} from './footer/footer-bottom/footer-bottom.component';
 import {SeoComponent} from '@shared/components/seo/seo.component';
 import {GadgetCardComponent} from '@shared/components/gadget-card/gadget-card.component';
 import {ProductCardComponent} from '@shared/components/product-card/product-card.component';
+import {TruncatePipe} from '@shared/pipes/truncate.pipe';
 
 const components = [
-    HeaderComponent,
-    FooterComponent,
-    HeaderTopComponent,
-    HeaderBottomComponent,
-    FooterTopComponent,
-    FooterBottomComponent,
-    SeoComponent,
-    GadgetCardComponent,
-    ProductCardComponent
+  HeaderComponent,
+  FooterComponent,
+  HeaderTopComponent,
+  HeaderBottomComponent,
+  FooterTopComponent,
+  FooterBottomComponent,
+  SeoComponent,
+  GadgetCardComponent,
+  ProductCardComponent
 ];
 
 @NgModule({
-    declarations   : components,
-    imports        : [
-        CommonModule
-    ],
-    exports        : components,
-    entryComponents: components,
+  declarations: [
+    components,
+    TruncatePipe
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: components,
+  entryComponents: components,
 })
 export class SharedComponentsModule {
 }
